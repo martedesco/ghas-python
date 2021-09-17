302de9085084f93f4098354ed5dff9dcb50596dc
Mathew Payne <2772944+GeekMasher@users.noreply.github.com>
2021-09-17 11:14:20 +0100

    Update cli.py

--- deserialization/cli.py
+++ deserialization/cli.py
@@ -1,9 +1,16 @@
 import yaml
+import pickle
 import argparse 
 
 parser = argparse.ArgumentParser(__name__)
-parser.add_argument('-i', '--input')
+parser.add_argument('-i', '--input', required=True)
 arguments = parser.parse_args()
 
-with open(arguments.input, 'r') as handle:
-    yaml.load(handle)
+
+if arguments.input.endswith('.yml'):
+    with open(arguments.input, 'r') as handle:
+        yaml.load(handle)
+
+elif arguments.input.endswith('.pickle'):
+    with open(arguments.input, 'r') as handle:
+        pickle.load(handle)
